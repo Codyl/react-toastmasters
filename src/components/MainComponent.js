a4851318dc5c8f865485feda617755c5e76360ca
Cody Lillywhite <codylillyw@gmail.com>
2021-11-17 20:50:34 -0500

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,11 +4,12 @@ import Directory from "./DirectoryComponent";
 import { CAMPSITES } from "../shared/campsites";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
-import Home from './HomeComponent';
+import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
 import { COMMENTS } from "../shared/comments";
 import { PARTNERS } from "../shared/partners";
 import { PROMOTIONS } from "../shared/promotions";
+import CampsiteInfo from "./CampsiteInfoComponent";
 
 class Main extends Component {
   constructor(props) {
@@ -34,15 +35,40 @@ class Main extends Component {
           partner={this.state.partners.filter((partner) => partner.featured)[0]}
         />
       );
-    }
+    };
+
+    const CampsiteWithId = ({ match }) => {
+      return (
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === +match.params.campsiteId
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.campsiteId === +match.params.campsiteId
+          )}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
-          <Route exact path="/directory" render={() => <Directory campsites={this.state.campsites} />} />
+          <Route
+            exact
+            path="/directory"
+            render={() => <Directory campsites={this.state.campsites} />}
+          />
+          <Route
+            exact
+            path="/directory/:campsiteId"
+            component={CampsiteWithId}
+          />
           <Route exact path="/contactus" component={Contact} />
-          <Redirect to="/home"/>
+          <Redirect to="/home" />
         </Switch>
         <Footer />
       </div>
