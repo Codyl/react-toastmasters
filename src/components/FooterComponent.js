77e85b74c61e18de721b8170143bd58d3d73d8ff
Cody Lillywhite <codylillyw@gmail.com>
2021-12-06 01:14:27 -0500

    Fix Footer information

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -13,49 +13,48 @@ function Footer(props) {
                 <Link to="/home">Home</Link>
               </li>
               <li>
-                <Link to="/directory">Directory</Link>
+                <a href="https://www.toastmasters.org/pathways-overview">
+                  Pathways
+                </a>
               </li>
               <li>
-                <Link to="/aboutus">About</Link>
+                <a href="https://zoom.us/j/455502545?pwd=cGJXUFVMK1RveFZkemZmYkxMb0x6dz09">
+                  Zoom
+                </a>
               </li>
               <li>
-                <Link to="/contactus">Contact</Link>
+                <a href="https://midwaytm.toastmastersclubs.org/">Old website</a>
               </li>
             </ul>
           </div>
           <div className="col-6 col-sm-3 text-center">
             <h5>Social</h5>
             <a
-              className="btn btn-social-icon btn-instagram"
-              href="http://instagram.com/">
-              <i className="fa fa-instagram" />
-            </a>{" "}
-            <a
               className="btn btn-social-icon btn-facebook"
-              href="http://www.facebook.com/">
+              href="https://www.facebook.com/Midwaycollegetoastmasters">
               <i className="fa fa-facebook" />
             </a>{" "}
             <a
               className="btn btn-social-icon btn-twitter"
-              href="http://twitter.com/">
+              href="https://twitter.com/midwaykytoastm1">
               <i className="fa fa-twitter" />
             </a>{" "}
             <a
               className="btn btn-social-icon btn-google"
-              href="http://youtube.com/">
+              href="https://www.youtube.com/channel/UCJCnYI2PsT6pXQkGtsh9D1w">
               <i className="fa fa-youtube" />
             </a>
           </div>
           <div className="col-sm-4 text-center">
-            <a role="button" className="btn btn-link" href="tel:+12065551234">
-              <i className="fa fa-phone" /> 1-206-555-1234
+            <a role="button" className="btn btn-link" href="tel:+8599832709">
+              <i className="fa fa-phone" /> 859-983-2709
             </a>
             <br />
             <a
               role="button"
               className="btn btn-link"
-              href="mailto:notreal@notreal.co">
-              <i className="fa fa-envelope-o" /> toastmastersMidway@changethislater.com
+              href="mailto:1990654@toastmastersclubs.com">
+              <i className="fa fa-envelope-o" /> 1990654@toastmastersclubs.com
             </a>
           </div>
         </div>
